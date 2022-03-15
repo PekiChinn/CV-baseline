7c34b89092431e7d8e0061088397715db1afd3d0
Peiji Chen <67468432+PekiChinn@users.noreply.github.com>
2022-03-15 14:28:01 +0900

    Add files via upload

---
+++ GoogleNet_V4/tools/common_tools.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+# @file name  : common_tools.py
+# @author     : TingsongYu https://github.com/TingsongYu
+# @date       : 2020-06-23
+# @brief      : 通用函数
+"""
+import numpy as np
+import torch
+import torch.nn as nn
+import os
+import random
+from PIL import Image
+from torch.utils.data import Dataset
+import matplotlib.pyplot as plt
+import torchvision.models as models
+from Paper.GoogleNet_V4.tools.inceptionv4 import inceptionv4
+from Paper.GoogleNet_V4.tools.inceptionresnetv2 import inceptionresnetv2
+
+
+def get_inception_v4(path_state_dict, device, vis_model=False):
+    """
+    创建模型，加载参数
+    :param path_state_dict:
+    :return:
+    """
+    model = inceptionv4(num_classes=1001, pretrained=None)
+    if path_state_dict:
+        pretrained_state_dict = torch.load(path_state_dict)
+        model.load_state_dict(pretrained_state_dict)
+    model.eval()
+
+    if vis_model:
+        from torchsummary import summary
+        summary(model, input_size=(3, 299, 299), device="cpu")
+
+    model.to(device)
+    return model
+
+
+def get_inception_resnet_v2(path_state_dict, device, vis_model=False):
+    """
+    创建模型，加载参数
+    :param path_state_dict:
+    :return:
+    """
+    model = inceptionresnetv2(num_classes=1001, pretrained=False)
+    if path_state_dict:
+        pretrained_state_dict = torch.load(path_state_dict)
+        model.load_state_dict(pretrained_state_dict)
+    model.eval()
+
+    if vis_model:
+        from torchsummary import summary
+        summary(model, input_size=(3, 299, 299), device="cpu")
+
+    model.to(device)
+    return model
+
+
+
+
---
+++ GoogleNet_V4/tools/inception-resnet-v2.py
@@ -0,0 +1,406 @@
+# -*- coding: utf-8 -*-
+"""
+# @file name   : inception-resnet-v2
+# @author      : PeiJi
+# @date        : 2022/1/4
+# @brief       :
+"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.model_zoo as model_zoo
+
+
+__all__ = ['InceptionResNetV2', 'inceptionresnetv2']
+
+pretrained_settings = {
+    'inceptionresnetv2': {
+        'imagenet': {
+            'url': 'http://data.lip6.fr/cadene/pretrainedmodels/inceptionresnetv2-520b38e4.pth',
+            'input_space': 'RGB',
+            'input_size': [3, 299, 299],
+            'input_range': [0, 1],
+            'mean': [0.5, 0.5, 0.5],
+            'std': [0.5, 0.5, 0.5],
+            'num_classes': 1000
+        },
+        'imagenet+background': {
+            'url': 'http://data.lip6.fr/cadene/pretrainedmodels/inceptionresnetv2-520b38e4.pth',
+            'input_space': 'RGB',
+            'input_size': [3, 299, 299],
+            'input_range': [0, 1],
+            'mean': [0.5, 0.5, 0.5],
+            'std': [0.5, 0.5, 0.5],
+            'num_classes': 1001
+        }
+    }
+}
+
+
+class BasicConv2d(nn.Module):
+
+    def __init__(self, in_planes, out_planes, kernel_size, stride, padding=0):
+        super(BasicConv2d, self).__init__()
+        self.conv = nn.Conv2d(in_planes, out_planes,
+                              kernel_size=kernel_size, stride=stride,
+                              padding=padding, bias=False)
+        self.bn = nn.BatchNorm2d(out_planes,
+                                 eps=0.001,
+                                 momentum=0.1,
+                                 affine=True)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        return x
+
+
+# 标准Inception module
+class Mixed_5b(nn.Module):
+
+    def __init__(self):
+        super(Mixed_5b, self).__init__()
+        self.branch0 = BasicConv2d(192, 96, kernel_size=1, stride=1)
+
+        self.branch1 = nn.Sequential(
+            BasicConv2d(192, 48, kernel_size=1, stride=1),
+            BasicConv2d(48, 64, kernel_size=5, stride=1, padding=2)
+        )
+
+        self.branch2 = nn.Sequential(
+            BasicConv2d(192, 64, kernel_size=1, stride=1),
+            BasicConv2d(64, 96, kernel_size=3, stride=1, padding=1),
+            BasicConv2d(96, 96, kernel_size=3, stride=1, padding=1)
+        )
+
+        self.branch3 = nn.Sequential(
+            nn.AvgPool2d(kernel_size=3, stride=1, padding=1, count_include_pad=False),
+            BasicConv2d(192, 64, kernel_size=1, stride=1)
+        )
+
+    def forward(self, x):
+        x0 = self.branch0(x)
+        x1 = self.branch1(x)
+        x2 = self.branch2(x)
+        x3 = self.branch3(x)
+        out = torch.cat((x0, x1, x2, x3), 1)       # 96+64+96+64 = 320
+        return out
+
+
+# Inception-Resnet-A figure 16.
+class Block35(nn.Module):
+
+    def __init__(self, scale=1.0):
+        super(Block35, self).__init__()
+        self.scale = scale
+
+        self.branch0 = BasicConv2d(320, 32, kernel_size=1, stride=1)
+
+        self.branch1 = nn.Sequential(
+            BasicConv2d(320, 32, kernel_size=1, stride=1),
+            BasicConv2d(32, 32, kernel_size=3, stride=1, padding=1)
+        )
+
+        self.branch2 = nn.Sequential(
+            BasicConv2d(320, 32, kernel_size=1, stride=1),
+            BasicConv2d(32, 48, kernel_size=3, stride=1, padding=1),
+            BasicConv2d(48, 64, kernel_size=3, stride=1, padding=1)
+        )
+
+        self.conv = nn.Conv2d(128, 320, kernel_size=1, stride=1)
+        self.relu = nn.ReLU(inplace=False)
+
+    def forward(self, x):
+        x0 = self.branch0(x)
+        x1 = self.branch1(x)
+        x2 = self.branch2(x)
+        out = torch.cat((x0, x1, x2), 1)
+        out = self.conv(out)
+        out = out * self.scale + x
+        out = self.relu(out)
+        return out
+
+
+# Reduction-A figure7.
+class Mixed_6a(nn.Module):
+
+    def __init__(self):
+        super(Mixed_6a, self).__init__()
+
+        self.branch0 = BasicConv2d(320, 384, kernel_size=3, stride=2)
+
+        self.branch1 = nn.Sequential(
+            BasicConv2d(320, 256, kernel_size=1, stride=1),
+            BasicConv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            BasicConv2d(256, 384, kernel_size=3, stride=2)
+        )
+
+        self.branch2 = nn.MaxPool2d(3, stride=2)
+
+    def forward(self, x):
+        x0 = self.branch0(x)
+        x1 = self.branch1(x)
+        x2 = self.branch2(x)
+        out = torch.cat((x0, x1, x2), 1)
+        return out
+
+
+# Inception-resnet-B figure17.
+class Block17(nn.Module):
+
+    def __init__(self, scale=1.0):
+        super(Block17, self).__init__()
+        self.scale = scale
+
+        self.branch0 = BasicConv2d(1088, 192, kernel_size=1,
+                                   stride=1)
+        self.branch1 = nn.Sequential(
+            BasicConv2d(1088, 128, kernel_size=1, stride=1),
+            BasicConv2d(128, 160, kernel_size=(1, 7), stride=1, padding=(0, 3)),
+            BasicConv2d(160, 192, kernel_size=(7, 1), stride=1, padding=(3, 0))
+        )
+
+        self.conv = nn.Conv2d(384, 1088, kernel_size=1, stride=1)
+        self.relu = nn.ReLU(inplace=False)
+
+    def forward(self, x):
+        x0 = self.branch0(x)
+        x1 = self.branch1(x)
+        out = torch.cat((x0, x1), 1)
+        out = self.conv(out)
+        out = out * self.scale + x
+        out = self.relu(out)
+        return out
+
+
+# Reduction-B figure 18.
+class Mixed_7a(nn.Module):
+
+    def __init__(self):
+        super(Mixed_7a, self).__init__()
+        self.branch0 = nn.MaxPool2d(kernel_size=3,
+                                    stride=2)
+        self.branch1 = nn.Sequential(
+            BasicConv2d(1088, 256, kernel_size=1, stride=1),
+            BasicConv2d(256, 384, kernel_size=3, stride=2)
+        )
+
+        self.branch2 = nn.Sequential(
+            BasicConv2d(1088, 256, kernel_size=1, stride=1),
+            BasicConv2d(256, 288, kernel_size=3, stride=2)
+        )
+
+        self.branch3 = nn.Sequential(
+            BasicConv2d(1088, 256, kernel_size=1, stride=1),
+            BasicConv2d(256, 288, kernel_size=3, stride=1, padding=1),
+            BasicConv2d(288, 320, kernel_size=3, stride=2)
+
+        )
+
+    def forward(self, x):
+        x0 = self.branch0(x)
+        x1 = self.branch1(x)
+        x2 = self.branch2(x)
+        x3 = self.branch3(x)
+        out = torch.cat((x0, x1, x2, x3), 1)
+        return out
+
+
+# Inception-C figure 19.
+# Inception-C figure 19.
+class Block8(nn.Module):
+
+    def __init__(self, scale=1.0, noReLU=False):
+        super(Block8, self).__init__()
+
+        self.scale = scale
+        self.noReLU = noReLU
+
+        self.branch0 = BasicConv2d(2080, 192, kernel_size=1, stride=1)
+
+        self.branch1 = nn.Sequential(
+            BasicConv2d(2080, 192, kernel_size=1, stride=1),
+            BasicConv2d(192, 224, kernel_size=(1, 3), stride=1, padding=(0, 1)),
+            BasicConv2d(224, 256, kernel_size=(3, 1), stride=1, padding=(1, 0))
+        )
+
+        self.conv2d = nn.Conv2d(448, 2080, kernel_size=1, stride=1)
+        if not self.noReLU:
+            self.relu = nn.ReLU(inplace=False)
+
+    def forward(self, x):
+        x0 = self.branch0(x)
+        x1 = self.branch1(x)
+        out = torch.cat((x0, x1), 1)
+        out = self.conv2d(out)
+        out = out * self.scale + x
+        if not self.noReLU:
+            out = self.relu(out)
+        return out
+
+
+# Inception Resnet v2
+class InceptionResNetV2(nn.Module):
+
+    def __init__(self, num_classes=101):
+        super(InceptionResNetV2, self).__init__()
+        self.input_space = None
+        self.input_size = (299, 299, 3)
+        self.mean = None
+        self.std = None
+
+        # Modules
+        # 1/6 Stem
+        self.conv2d_1a = BasicConv2d(3, 32, kernel_size=3, stride=2)
+        self.conv2d_2a = BasicConv2d(32, 32, kernel_size=3, stride=1)
+        self.conv2d_2b = BasicConv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.maxpool_3a = nn.MaxPool2d(3, stride=2)
+        self.conv2d_3b = BasicConv2d(64, 80, kernel_size=1, stride=1)
+        self.conv2d_4a = BasicConv2d(80, 192, kernel_size=3, stride=1)
+        self.maxpool_5a = nn.MaxPool2d(3, stride=2)
+
+        # 2/6
+        self.mixed_5b = Mixed_5b()
+        self.repeat = nn.Sequential(
+            Block35(scale=0.17),
+            Block35(scale=0.17),
+            Block35(scale=0.17),
+            Block35(scale=0.17),
+            Block35(scale=0.17),
+            Block35(scale=0.17),
+            Block35(scale=0.17),
+            Block35(scale=0.17),
+            Block35(scale=0.17),
+            Block35(scale=0.17),
+        )
+
+        # 3/6 Reduction-A figure7.
+        self.mixed_6a = Mixed_6a()
+
+        # 4/6 Inception-resnet-B * 20
+        self.repeat_1 = nn.Sequential(
+            Block17(scale=0.10),
+            Block17(scale=0.10),
+            Block17(scale=0.10),
+            Block17(scale=0.10),
+            Block17(scale=0.10),
+            Block17(scale=0.10),
+            Block17(scale=0.10),
+            Block17(scale=0.10),
+            Block17(scale=0.10),
+            Block17(scale=0.10),
+            Block17(scale=0.10),
+            Block17(scale=0.10),
+            Block17(scale=0.10),
+            Block17(scale=0.10),
+            Block17(scale=0.10),
+            Block17(scale=0.10),
+            Block17(scale=0.10),
+            Block17(scale=0.10),
+            Block17(scale=0.10),
+            Block17(scale=0.10),
+        )
+
+        # 5/6 Reduction-B figure 18. 8*8*1792
+        self.mixed_7a = Mixed_7a()
+
+        # 6/6 Inception-C * 9 figure 19.
+        self.repeat_2 = nn.Sequential(
+            Block8(scale=0.20),
+            Block8(scale=0.20),
+            Block8(scale=0.20),
+            Block8(scale=0.20),
+            Block8(scale=0.20),
+            Block8(scale=0.20),
+            Block8(scale=0.20),
+            Block8(scale=0.20),
+            Block8(scale=0.20),
+        )
+        self.block8 = Block8(noReLU=True)
+        self.conv2d_7b = BasicConv2d(2080, 1536, kernel_size=1, stride=1)
+        self.avgpool_1a = nn.AvgPool2d(8, count_include_pad=False)
+        self.last_linear = nn.Linear(1536, num_classes)
+
+    def features(self, input):
+
+        # 1/6 Stem: figure 14.
+        x = self.conv2d_1a(input)   # 149*149*32
+        x = self.conv2d_2a(x)       # 147*147*32
+        x = self.conv2d_2b(x)       # 149*149*64
+        x = self.maxpool_3a(x)      # 73*73*64
+        x = self.conv2d_3b(x)       # 73*73*80
+        x = self.conv2d_4a(x)       # 71*71*192
+        x = self.maxpool_5a(x)      # 35*35*192
+
+        # 2/6 Inception-resnet-A： figure 16.
+        x = self.mixed_5b(x)        # 35*35*320 标准Inception moudle, 额外增加的
+        x = self.repeat(x)          # 35*35*320 论文是35*35*384
+
+        # 3/6 Reduction-A  figure7.
+        x = self.mixed_6a(x)        # 17*17*1088
+
+        # 4/6 Inception-resnet-B  figure17.
+        x = self.repeat_1(x)        # 17*17*1088
+
+        # 5/6 Reduction-B figure 18.
+        x = self.mixed_7a(x)        # 8*8*2080
+
+        # 6/6 Inception-C figure 19.
+        x = self.repeat_2(x)        # 8*8*2080
+        x = self.block8(x)          # 该模块输出前未用Relu，原因未知
+
+        # 1*1卷积压缩特征图厚度：2080 --> 1536
+        x = self.conv2d_7b(x)       # 8*8*1536
+
+        return x
+
+    def logits(self, features):
+        x = self.avgpool_1a(features)       # 1*1*1536
+        x = x.view(x.size(0), -1)           # 1536
+        x = self.last_linear(x)             # 1000
+        return x
+
+    def forward(self, input):
+        x = self.features(input)
+        x = self.logits(x)
+        return x
+
+
+# 创建模型的函数
+def inceptionresnetv2(num_classes=1000, pretrained='imagenet'):
+    r"""InceptionResNetV2 model architecture from the
+    `"InceptionV4, Inception-ResNet..." <https://arxiv.org/abs/1602.07261>`_ paper.
+    """
+    if pretrained:
+        settings = pretrained_settings['inceptionresnetv2'][pretrained]
+        assert num_classes == settings['num_classes'], \
+            "num_classes should be {}, but is {}".format(settings['num_classes'], num_classes)
+
+        # both 'imagenet'&'imagenet+background' are loaded from same parameters
+        model = InceptionResNetV2(num_classes=1001)
+        model.load_state_dict(model_zoo.load_url(settings['url']))
+
+        if pretrained == 'imagenet':
+            new_last_linear = nn.Linear(1536, 1000)
+            new_last_linear.weight.data = model.last_linear.weight.data[1:]
+            new_last_linear.bias.data = model.last_linear.bias.data[1:]
+            model.last_linear = new_last_linear
+
+        model.input_space = settings['input_space']
+        model.input_size = settings['input_size']
+        model.input_range = settings['input_range']
+
+        model.mean = settings['mean']
+        model.std = settings['std']
+    else:
+        model = InceptionResNetV2(num_classes=num_classes)
+    return model
+
+
+if __name__ == '__main__':
+    inceptionresnet_v2_model = inceptionresnetv2(num_classes=1001, pretrained=False)
+    fake_data = torch.randn((2, 3, 299, 299))
+    outputs = inceptionresnet_v2_model(fake_data)
+    print(outputs)
